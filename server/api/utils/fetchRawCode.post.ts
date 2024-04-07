9c02acfaa1b706f1d4f643cb5b05478fbfe7310f
Fan <fan@MacBook-Pro.local>
2024-04-07 10:03:00 +0800

    fix: update raw code url

--- server/api/utils/fetchRawCode.post.ts
+++ server/api/utils/fetchRawCode.post.ts
@@ -2,9 +2,9 @@
  * 从 Gitlab 上抓取代码的 Raw 文件
  */
 export default defineEventHandler(async (event) => {
-  const gitlabFileUrl = 'http://10.102.81.50/gitlab/admin3/shimo-api/-/raw/main/src/pages'
+  const gitFileUrl = 'https://raw.githubusercontent.com/vampirefan/shimo-api/main/src/pages'
   const body = await readBody(event)
-  const fileUrl = `${gitlabFileUrl + body.path}.vue`
+  const fileUrl = `${gitFileUrl + body.path}.vue`
   const fileContent = await $fetch(fileUrl)
   return fileContent
 })
